f4b409498a9debcd5e25d5da69e55966a9c9cc06
angel <passerby365@163.com>
2025-06-17 13:21:25 +0800

    修复: 简化bin文件

--- bin/code-insight.js
+++ bin/code-insight.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-try {
-  import('../dist/cli/index.js');
-} catch (error) {
+
+// 导入并执行CLI
+import('../dist/cli/index.js').catch((error) => {
   console.error('执行失败:', error);
   process.exit(1);
-}
+});
